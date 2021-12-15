aff95cbbada94228372b3a67606d5c6b041e5e95
Rob <s100934@ap.be>
2021-12-15 16:37:52 +0100

    Create Carlo.py

---
+++ Labo4/project/chess_agents/Carlo.py
@@ -0,0 +1,25 @@
+from abc import ABC
+import chess
+import math
+from project.chess_utilities.utility import Utility
+
+"""A generic agent class"""
+
+class Carlo(ABC):
+
+    def __init__(self, utility: Utility, time_limit_move: float) -> None:
+        """Setup the Search Agent"""
+        self.utility = utility
+        self.time_limit_move = time_limit_move
+        self.state = chess.Board()
+        self.children = None
+        self.parent = set()
+        self.N = 0
+        self.U = 0
+
+    def calculate_move(self, board: chess.Board):
+        pass
+    def UCB1(CurrentCarlo : Carlo):
+        return (self.U/self.N) + 1.5 * math.sqrt(log(parent.N))
+    def selection():
+        
